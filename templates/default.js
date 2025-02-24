d0bdb6e0391dc948fbaab9179ecb2007be46023f
morfen-mor <mor.tayeb@inthegame.io>
2025-02-24 11:00:27 +0200

    change links

--- templates/default.js
+++ templates/default.js
@@ -1,7 +1,7 @@
 window.clientTemplates = {
     poll: 'http://assets.internal.inthegame.io/templates/general/poll.json',
-    quiz: 'https://d2jn6lssoj32qo.cloudfront.net/general/creatives/v2-6/creative_lband_aws.json',
-    lband: 'http://assets.internal.inthegame.io/templates/general/lband elemets.json',
-    split: 'https://d2jn6lssoj32qo.cloudfront.net/general/creatives/v2-6/creative_animation_giftdazn.json',
+    quiz: 'http://assets.internal.inthegame.io/templates/general/quiz.json'
+    lband: 'http://assets.internal.inthegame.io/templates/general/lband.json',
+    split: 'http://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'http://assets.internal.inthegame.io/templates/general/banner.json'
 }; 
