41bfb58f435d2de38158aacab8139bdb4d718562
morfen-mor <mor.tayeb@inthegame.io>
2025-02-24 12:06:18 +0200

    added comma

--- templates/default.js
+++ templates/default.js
@@ -1,6 +1,6 @@
 window.clientTemplates = {
     poll: 'http://assets.internal.inthegame.io/templates/general/poll.json',
-    quiz: 'http://assets.internal.inthegame.io/templates/general/quiz.json'
+    quiz: 'http://assets.internal.inthegame.io/templates/general/quiz.json',
     lband: 'http://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'http://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'http://assets.internal.inthegame.io/templates/general/banner.json'
