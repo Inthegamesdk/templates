6d067c234e628c2db9eec3262fefddb620657c72
morfen-mor <mor.tayeb@inthegame.io>
2025-02-24 14:00:31 +0200

    1

--- templates.js
+++ templates.js
@@ -1,5 +1,6 @@
 const templates = {
-    poll: 'http://assets.internal.inthegame.io/templates/general/poll.json',
+    poll3: 'http://assets.internal.inthegame.io/templates/general/poll.json',
+    poll2: 'http://assets.internal.inthegame.io/templates/general/poll2.json',
     quiz: 'https://d2jn6lssoj32qo.cloudfront.net/general/creatives/v2-6/creative_lband_aws.json',
     lband: 'http://assets.internal.inthegame.io/templates/general/lband elemets.json',
     split: 'https://d2jn6lssoj32qo.cloudfront.net/general/creatives/v2-6/creative_animation_giftdazn.json',
