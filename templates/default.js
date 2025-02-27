7fedd969dedf1da4498c88358995265fedd251eb
itaiarbel1 <itai@inthegame.io>
2025-02-27 15:51:50 +0200

    https

--- templates/default.js
+++ templates/default.js
@@ -1,8 +1,8 @@
 window.clientTemplates = {
-    poll3: 'http://assets.internal.inthegame.io/templates/general/poll.json',
-    poll2: 'http://assets.internal.inthegame.io/templates/general/poll2.json',
-    quiz: 'http://assets.internal.inthegame.io/templates/general/quiz.json',
-    lband: 'http://assets.internal.inthegame.io/templates/general/lband.json',
-    split: 'http://assets.internal.inthegame.io/templates/general/splitscreen.json',
-    banner: 'http://assets.internal.inthegame.io/templates/general/banner.json'
+    poll3: 'https://assets.internal.inthegame.io/templates/general/poll.json',
+    poll2: 'https://assets.internal.inthegame.io/templates/general/poll2.json',
+    quiz: 'https://assets.internal.inthegame.io/templates/general/quiz.json',
+    lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
+    split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
+    banner: 'https://assets.internal.inthegame.io/templates/general/banner.json'
 }; 
