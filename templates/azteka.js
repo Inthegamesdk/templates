0df0c01b0ee4ede7c22522bab8423c5c58168f94
morfen-mor <mor.tayeb@inthegame.io>
2025-02-27 15:33:08 +0200

    add azteka templates

--- templates/azteka.js
+++ templates/azteka.js
@@ -1,5 +1,5 @@
 window.clientTemplates = {
-    quiz: 'https://d2jn6lssoj32qo.cloudfront.net/general/creatives/v2-6/creative_lband_aws.json',
-    lband: 'http://assets.internal.inthegame.io/templates/general/lband elemets.json',
-    poll: 'http://assets.internal.inthegame.io/templates/general/poll.json'
+    sidebar_video: 'http://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.json',
+    lband: 'http://assets.internal.inthegame.io/templates/azteka/lband_azteka.json',
+    splitscreen: 'http://assets.internal.inthegame.io/templates/azteka/splitscreen_azteka.json'
 }; 
--- templates/default.js
+++ templates/default.js
@@ -1,5 +1,6 @@
 window.clientTemplates = {
-    poll: 'http://assets.internal.inthegame.io/templates/general/poll.json',
+    poll3: 'http://assets.internal.inthegame.io/templates/general/poll.json',
+    poll2: 'http://assets.internal.inthegame.io/templates/general/poll2.json',
     quiz: 'http://assets.internal.inthegame.io/templates/general/quiz.json',
     lband: 'http://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'http://assets.internal.inthegame.io/templates/general/splitscreen.json',
