01eec4ab7bea3af85b138074e0e1e18cbfbf0a73
morfen-mor <mor.tayeb@inthegame.io>
2025-03-02 17:15:48 +0200

    added templates

--- templates/azteka.js
+++ templates/azteka.js
@@ -1,5 +1,8 @@
 window.clientTemplates = {
     sidebar_video: 'https://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.json',
     lband: 'https://assets.internal.inthegame.io/templates/azteka/lband_azteka.json',
-    splitscreen: 'https://assets.internal.inthegame.io/templates/azteka/splitscreen_azteka.json'
+    splitscreen: 'https://assets.internal.inthegame.io/templates/azteka/splitscreen_azteka.json',
+    quiz_web: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web.json',
+    quiz_web_correct: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct.json',
+    quiz_web_incorrect: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect'
 }; 
