2baa7963af43f54476b7482ea04a34f3b0edc666
morfen-mor <mor.tayeb@inthegame.io>
2025-03-03 13:33:11 +0200

    update links

--- templates/default.js
+++ templates/default.js
@@ -1,7 +1,6 @@
 window.clientTemplates = {
-    poll3: 'https://assets.internal.inthegame.io/templates/general/poll.json',
-    poll2: 'https://assets.internal.inthegame.io/templates/general/poll2.json',
-    quiz: 'https://assets.internal.inthegame.io/templates/general/quiz.json',
+    
+    
     lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'https://assets.internal.inthegame.io/templates/general/banner.json'
