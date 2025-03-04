038b12cef663df511d31a47d6df0060abcb49559
morfen-mor <mor.tayeb@inthegame.io>
2025-03-04 15:31:28 +0200

    added viu

--- templates/azteka.js
+++ templates/azteka.js
@@ -1,6 +1,6 @@
 window.clientTemplates = {
     sidebar_video: 'https://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.json',
-    lband: 'https://assets.internal.inthegame.io/templates/azteka/lband_azteka.json',
+    lband: 'https://https://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.jsonassets.internal.inthegame.io/templates/azteka/lband_azteka.json',
     splitscreen: 'https://assets.internal.inthegame.io/templates/azteka/splitscreen_azteka.json',
     quiz_web: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web.json',
     quiz_web_correct: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct.json',
---
+++ templates/viu.js
@@ -0,0 +1,5 @@
+window.clientTemplates = {
+    
+    lband: 'https://assets.internal.inthegame.io/templates/viu/viu_lband',
+    product_lband: 'https://assets.internal.inthegame.io/templates/viu/viu_lband'
+}; 
