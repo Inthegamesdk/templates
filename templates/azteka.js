595a41d1abb6fccb4da5daee66cf8e69791a6f60
morfen-mor <mor.tayeb@inthegame.io>
2025-03-04 11:46:22 +0200

    link update

--- templates/azteka.js
+++ templates/azteka.js
@@ -5,6 +5,6 @@ window.clientTemplates = {
     quiz_web: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web.json',
     quiz_web_correct: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct.json',
     quiz_web_incorrect: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect.json',
-    quiz_web_correct_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct_no_Variables',
-    quiz_web_incorrect_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect_no_Variables'
+    quiz_web_correct_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct_no_Variables.json',
+    quiz_web_incorrect_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect_no_Variables.json'
 }; 
