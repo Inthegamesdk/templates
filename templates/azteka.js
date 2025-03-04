7b88925c6e5710fa420572fde844616984f3e048
morfen-mor <mor.tayeb@inthegame.io>
2025-03-04 12:24:10 +0200

    add template

--- templates/azteka.js
+++ templates/azteka.js
@@ -6,5 +6,6 @@ window.clientTemplates = {
     quiz_web_correct: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct.json',
     quiz_web_incorrect: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect.json',
     quiz_web_correct_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct_no_Variables.json',
-    quiz_web_incorrect_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect_no_Variables.json'
+    quiz_web_incorrect_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect_no_Variables.json',
+    quiz_web_Question_answers: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_Q&A.json'
 }; 
