097df2b3e58fe3428946ee97fe621cf5b49ecfd2
morfen-mor <mor.tayeb@inthegame.io>
2025-03-05 11:39:49 +0200

    update template

--- templates/viu.js
+++ templates/viu.js
@@ -1,5 +1,5 @@
 window.clientTemplates = {
     
     lband: 'https://assets.internal.inthegame.io/templates/viu/viu_lband.json',
-    product_lband: 'https://assets.internal.inthegame.io/templates/viu/viu_lband.json'
+    product_lband: 'https://assets.internal.inthegame.io/templates/viu/template_lband_product.json'
 }; 
