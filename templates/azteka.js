0647392e0342b0e31659cc3b009c8edafc81dce1
morfen-mor <mor.tayeb@inthegame.io>
2025-03-11 12:35:39 +0200

    update templates

--- templates/azteka.js
+++ templates/azteka.js
@@ -1,12 +1,10 @@
 window.clientTemplates = {
     sidebar_video: 'https://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.json',
-    lband: 'https://https://assets.internal.inthegame.io/templates/azteka/sidebar_video_azteka.jsonassets.internal.inthegame.io/templates/azteka/lband_azteka.json',
+    lband: 'https://assets.internal.inthegame.io/templates/azteka/lband_azteka.json',
     splitscreen: 'https://assets.internal.inthegame.io/templates/azteka/splitscreen_azteka.json',
     quiz_web: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web.json',
     quiz_web_correct: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct.json',
     quiz_web_incorrect: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect.json',
-    quiz_web_correct_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_correct_no_Variables.json',
-    quiz_web_incorrect_original: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_incorrect_no_Variables.json',
     quiz_web_Question_answers: 'https://assets.internal.inthegame.io/templates/azteka/azteka_quiz_web_Q&A.json',
     quiz_web_correct_team_odds: 'https://assets.internal.inthegame.io/templates/azteka/azteka_demo_correct_web_team&odds.json',
     quiz_web_incorrect_team_odds: 'https://assets.internal.inthegame.io/templates/azteka/azteka_demo_incorrect__web_team&odds.json',
