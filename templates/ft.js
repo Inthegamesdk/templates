b44d53d83d8cfdd86c8ef6261406217d76e74700
morfen-mor <mor.tayeb@inthegame.io>
2025-03-11 15:34:03 +0200

    update templates

--- templates/ft.js
+++ templates/ft.js
@@ -1,4 +1,5 @@
 window.clientTemplates = {
-    poll: 'http://assets.internal.inthegame.io/templates/general/poll.json',
-    banner: 'http://assets.internal.inthegame.io/templates/general/banner.json'
+    lband: 'https://assets.internal.inthegame.io/templates/FT/lband.json',
+    full_screen_ad: 'https://assets.internal.inthegame.io/templates/FT/full_screen_ad.json',
+    side_pause_ad: 'https://assets.internal.inthegame.io/templates/FT/side_pause_ad.json'
 }; 
