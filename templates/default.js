3709ebf223ecb7d4ea1a75a7323373f127a4e06c
morfen-mor <mor.tayeb@inthegame.io>
2025-03-30 20:04:16 +0300

    update links

--- templates/default.js
+++ templates/default.js
@@ -3,5 +3,7 @@ window.clientTemplates = {
     
     lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
-    banner: 'https://assets.internal.inthegame.io/templates/general/banner.json'
+    banner: 'https://assets.internal.inthegame.io/templates/general/banner.json',
+    lower_third_poll: 'https://assets.internal.inthegame.io/templates/general/ft_lower_third_text_images.json',
+    lower_third_feedback: 'https://assets.internal.inthegame.io/templates/general/feedback_lower_third_text_images.json'
 }; 
