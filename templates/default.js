ba89d16f8c3e2128a774e3ad67c2848fc35dbc8a
morfen-mor <mor.tayeb@inthegame.io>
2025-03-31 09:51:53 +0300

    update links

--- templates/default.js
+++ templates/default.js
@@ -4,6 +4,5 @@ window.clientTemplates = {
     lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'https://assets.internal.inthegame.io/templates/general/banner.json',
-    lower_third_poll: 'https://assets.internal.inthegame.io/templates/general/ft_lower_third_text_images.json',
-    lower_third_feedback: 'https://assets.internal.inthegame.io/templates/general/feedback_lower_third_text_images.json'
+
 }; 
--- templates/ft.js
+++ templates/ft.js
@@ -1,5 +1,7 @@
 window.clientTemplates = {
     lband: 'https://assets.internal.inthegame.io/templates/FT/lband.json',
     full_screen_ad: 'https://assets.internal.inthegame.io/templates/FT/full_screen_ad.json',
-    side_pause_ad: 'https://assets.internal.inthegame.io/templates/FT/side_pause_ad.json'
+    side_pause_ad: 'https://assets.internal.inthegame.io/templates/FT/side_pause_ad.json',
+    lower_third_poll: 'https://assets.internal.inthegame.io/templates/FT/ft_lower_third_text_images.json',
+    lower_third_feedback: 'https://assets.internal.inthegame.io/templates/FT/feedback_lower_third_text_images.json'
 }; 
