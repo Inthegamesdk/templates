f196549f9c276598fe8050469b869742f1642832
morfen-mor <mor.tayeb@inthegame.io>
2025-04-02 13:21:22 +0300

    added templates

--- templates/ft.js
+++ templates/ft.js
@@ -3,5 +3,7 @@ window.clientTemplates = {
     full_screen_ad: 'https://assets.internal.inthegame.io/templates/FT/full_screen_ad.json',
     side_pause_ad: 'https://assets.internal.inthegame.io/templates/FT/side_pause_ad.json',
     lower_third_poll: 'https://assets.internal.inthegame.io/templates/FT/ft_lower_third_text_images.json',
-    lower_third_feedback: 'https://assets.internal.inthegame.io/templates/FT/feedback_lower_third_text_images.json'
+    lower_third_feedback: 'https://assets.internal.inthegame.io/templates/FT/feedback_lower_third_text_images.json',
+    product_lband: 'https://assets.internal.inthegame.io/templates/FT/lband_product.json',
+    split_screen: 'https://assets.internal.inthegame.io/templates/FT/splitscreen.json'
 }; 
