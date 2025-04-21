3a408fdac4d025f3bf3c1b29a64c4e575a344682
morfen-mor <mor.tayeb@inthegame.io>
2025-04-21 14:12:34 +0300

    aaded links

--- templates/default.js
+++ templates/default.js
@@ -4,5 +4,5 @@ window.clientTemplates = {
     lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'https://assets.internal.inthegame.io/templates/general/banner.json',
-
+    
 }; 
---
+++ templates/kargo.js
@@ -0,0 +1,5 @@
+window.clientTemplates = {
+    lower_third: 'https://assets.internal.inthegame.io/templates/kargo/kargo_lower.json',
+    full_screen_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_full.json',
+    side_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_side.json'
+}; 
