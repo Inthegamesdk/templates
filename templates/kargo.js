ef730314e3a1b070a4bcfef9d2aeed19f39f7afd
morfen-mor <mor.tayeb@inthegame.io>
2025-04-22 13:34:47 +0300

    added link

--- templates/kargo.js
+++ templates/kargo.js
@@ -1,5 +1,6 @@
 window.clientTemplates = {
     lower_third: 'https://assets.internal.inthegame.io/templates/kargo/kargo_lower.json',
     full_screen_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_full.json',
-    side_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_side.json'
+    right_side_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_side.json',
+    left_side_ad: 'https://assets.internal.inthegame.io/templates/kargo/karo_left_side.json'
 }; 
