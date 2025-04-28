5a4ac5676f1fc89c788641db61aa585af4f7c65c
itaiarbel1 <itai@inthegame.io>
2025-04-28 18:31:08 +0300

    change

--- templates/kargo.js
+++ templates/kargo.js
@@ -1,9 +1,9 @@
 window.clientTemplates = {
+    fullscreen_animation: 'https://assets.internal.inthegame.io/templates/general/full_ad_all.json',
     lower_third: 'https://assets.internal.inthegame.io/templates/kargo/kargo_lower.json',
     full_screen_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_full.json',
     right_side_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_side.json',
     left_side_ad: 'https://assets.internal.inthegame.io/templates/kargo/karo_left_side.json',
     lower_third_animation: 'https://assets.internal.inthegame.io/templates/general/lower_third_animation.json',
-    left_side_animation: 'https://assets.internal.inthegame.io/templates/general/left_side_animation.json',
-    full_animation_only: 'https://assets.internal.inthegame.io/templates/general/full_ad_all.json',
+    left_side_animation: 'https://assets.internal.inthegame.io/templates/general/left_side_animation.json'
 }; 
