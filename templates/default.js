0c47f06864b82e26c1c3d314337acb9aacee6b2e
morfen-mor <mor.tayeb@inthegame.io>
2025-04-28 12:38:25 +0300

    added links

--- templates/default.js
+++ templates/default.js
@@ -4,5 +4,9 @@ window.clientTemplates = {
     lband: 'https://assets.internal.inthegame.io/templates/general/lband.json',
     split: 'https://assets.internal.inthegame.io/templates/general/splitscreen.json',
     banner: 'https://assets.internal.inthegame.io/templates/general/banner.json',
-    
+    lower_third_animation: 'https://assets.internal.inthegame.io/templates/general/lower_third_animation.json',
+    right_side_animation: 'https://assets.internal.inthegame.io/templates/general/right_side_animation.json',
+    left_side_animation: 'https://assets.internal.inthegame.io/templates/general/left_side_animation.json',
+    full_animation_only: 'https://assets.internal.inthegame.io/templates/general/full_ad_all.json',
+    full_animation_with_bg: 'https://assets.internal.inthegame.io/templates/general/full_ad_bg_video.json',
 }; 
