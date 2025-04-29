ab41fd112a67cae5d85a0bd4a6d6f37769b4b0a9
morfen-mor <mor.tayeb@inthegame.io>
2025-04-29 11:30:55 +0300

    updated links

--- templates/kargo.js
+++ templates/kargo.js
@@ -1,6 +1,7 @@
 window.clientTemplates = {
-    fullscreen_animation: 'https://assets.internal.inthegame.io/templates/general/full_ad_all.json',
-    left_side_animation: 'https://assets.internal.inthegame.io/templates/general/left_side_animation.json',
+    fullscreen_animation: 'https://assets.internal.inthegame.io/templates/kargo/full_ad_all.json',
+    left_side_animation: 'https://assets.internal.inthegame.io/templates/kargo/left_side_animation.json',
+    right_side_animation: 'https://assets.internal.inthegame.io/templates/kargo/right_side_animation.json',
     lower_third: 'https://assets.internal.inthegame.io/templates/kargo/kargo_lower.json',
     full_screen_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_full.json',
     right_side_ad: 'https://assets.internal.inthegame.io/templates/kargo/kargo_side.json',
