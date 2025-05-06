ad0ac2b1e72a9fb8cf13db6a14831217fdc87e24
morfen-mor <mor.tayeb@inthegame.io>
2025-05-06 14:48:44 +0300

    update links

---
+++ templates/NowTV.js
@@ -0,0 +1,5 @@
+window.clientTemplates = {
+   Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_right.json',
+   Lband_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json'
+    
+}; 
