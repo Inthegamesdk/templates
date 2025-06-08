bfda432ed7f91fa327aeb5dea9624c54f624dc7a
morfen-mor <mor.tayeb@inthegame.io>
2025-06-08 14:26:36 +0300

    added links

---
+++ templates/futuretoday_external.js
@@ -0,0 +1,7 @@
+window.clientTemplates = {
+    lband: 'https://assets.internal.inthegame.io/templates/FT/external/external_lband.json',
+    side_ad: 'https://assets.internal.inthegame.io/templates/FT/external/external_side_ad',
+    lower_third_ad: 'https://assets.internal.inthegame.io/templates/FT/external/external_lower_third_banner.json'
+}; 
+
+
