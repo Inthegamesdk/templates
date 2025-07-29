8eaa352894ba3757f25192802d19261acf23029e
morfen-mor <mor.tayeb@inthegame.io>
2025-07-29 10:02:34 +0300

    update link

--- templates/verve.js
+++ templates/verve.js
@@ -1,5 +1,5 @@
 window.clientTemplates = {
    Lband: 'https://assets.internal.inthegame.io/templates/verve/verve_lband.json',
-   Right_side_ad: 'https://assets.internal.inthegame.io/templates//verve/verve_right_side.json',
+   Right_side_ad: 'https://assets.internal.inthegame.io/templates/verve/verve_right_side.json',
    Corner_ad_animation: 'https://assets.internal.inthegame.io/templates/verve/verve_side_bottom_animation.json'
 }; 
