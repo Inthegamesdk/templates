10928281eec8c9a51569e8703c854170964d0c9c
morfen-mor <mor.tayeb@inthegame.io>
2025-10-16 15:50:19 +0300

    update links

--- templates/NowTV.js
+++ templates/NowTV.js
@@ -1,5 +1,7 @@
 window.clientTemplates = {
    Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_right.json',
-   Lband_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json'
-    
+   Lband_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json',
+   Double_Box: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box.json', 
+   Lower_Banner: 'https://assets.internal.inthegame.io/nowtv_overlay_bottom.json', 
+   Pause_Ad_Side: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad.json'
 }; 
