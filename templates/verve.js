7e258ef758d331e14834578d10f4646751bc7b9b
morfen-mor <mor.tayeb@inthegame.io>
2025-10-16 13:26:43 +0300

    updatelinks

--- templates/verve.js
+++ templates/verve.js
@@ -2,8 +2,9 @@ window.clientTemplates = {
    Lband: 'https://assets.internal.inthegame.io/templates/verve/verve_lband.json',
    Right_side_ad: 'https://assets.internal.inthegame.io/templates/verve/verve_right_side.json',
    Corner_ad_animation: 'https://assets.internal.inthegame.io/templates/verve/verve_side_bottom_animation.json',
-   full_video_ad: 'https://assets.internal.inthegame.io/templates/verve/verve_full_video_ad.json',
-   full_image_ad: 'https://assets.internal.inthegame.io/templates/verve/verve_full_ad_image.json',
-   full_video_ad_no_button: 'https://assets.internal.inthegame.io/templates/verve/verve_full_video_no_button.json',
+   full_video_with_button: 'https://assets.internal.inthegame.io/templates/verve/verve_full_video_ad.json',
+   full_image_with_button: 'https://assets.internal.inthegame.io/templates/verve/verve_full_ad_image.json',
+   full_video_no_button: 'https://assets.internal.inthegame.io/templates/verve/verve_full_video_no_button.json',
+   full_image_no_button: 'https://assets.internal.inthegame.io/templates/verve/verve_full_ad_image_no_button',
    left_side_ad: 'https://assets.internal.inthegame.io/templates/verve/verve_left_side.json'
 }; 
