1a8ce7041cd527a566614d9111bcf7951ee8155c
morfen-mor <mor.tayeb@inthegame.io>
2025-10-23 17:05:15 +0300

    update links

--- templates/NowTV.js
+++ templates/NowTV.js
@@ -2,6 +2,11 @@ window.clientTemplates = {
    Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_right.json',
    Lband_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json',
    Double_Box: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box.json', 
-   Lower_Banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom.json', 
-   Pause_Ad_Side: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad.json'
+   Double_Box_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box_size_positions.json', 
+   Lower_Banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom.json',
+   Lower_Banner_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom_size_positions.json',  
+   Pause_Ad_Side: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad.json',
+   Pause_Ad_Side_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad_size_positions.json',
+   Mobile_Web_Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_lband.json',
+   Mobile_Web_ovelry_banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_lband.json'
 }; 
