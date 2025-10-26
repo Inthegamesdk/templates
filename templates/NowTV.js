97ac09d1342fc78d87de05786512114556cbf430
morfen-mor <mor.tayeb@inthegame.io>
2025-10-26 13:45:03 +0200

    update links

--- templates/NowTV.js
+++ templates/NowTV.js
@@ -1,12 +1,13 @@
 window.clientTemplates = {
-   Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_right.json',
-   Lband_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json',
-   Double_Box: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box.json', 
-   Double_Box_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box_size_positions.json', 
-   Lower_Banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom.json',
-   Lower_Banner_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom_size_positions.json',  
+   Lband_TV: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_right.json',
+   Lband_TV_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_lband_video_size_positions.json',
+   Double_Box_TV: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box.json', 
+   Double_Box_TV_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_double_box_size_positions.json', 
+   Ovelry_banner_TV: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom.json',
+   Ovelry_banner_TV_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_overlay_bottom_size_positions.json',  
    Pause_Ad_Side: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad.json',
    Pause_Ad_Side_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_pause_side_ad_size_positions.json',
    Mobile_Web_Lband: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_lband.json',
-   Mobile_Web_ovelry_banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_overlay.json'
-}; 
+   Mobile_Web_ovelry_banner: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_overlay.json',
+   Mobile_Web_ovelry_banner_positions_size: 'https://assets.internal.inthegame.io/templates/NowTV/nowtv_web_mobile_overlay_size_positions.json'
+};
