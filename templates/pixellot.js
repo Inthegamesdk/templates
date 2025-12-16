8b006f68c2606cc5a6bb6345deb896b00a8202ae
morfen-mor <mor.tayeb@inthegame.io>
2025-12-16 12:21:16 +0200

    updated links

--- templates/pixellot.js
+++ templates/pixellot.js
@@ -1,7 +1,6 @@
 window.clientTemplates = {
     
     lband: 'https://assets.internal.inthegame.io/templates/pixellot/pixellot_lband.json',
-    doubel_box: 'https://assets.internal.inthegame.io/templates/pixellot/pixellot_double_box.json',
+    double_box: 'https://assets.internal.inthegame.io/templates/pixellot/pixellot_double_box.json',
     side_ad: 'https://assets.internal.inthegame.io/templates/pixellot/pixellot_right_side.json'
-
 }; 
